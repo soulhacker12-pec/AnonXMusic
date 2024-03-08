6beb61ffc421f49b030195b6ed8a36bc4b9dca5e
soulhacker12-pec <155738175+soulhacker12-pec@users.noreply.github.com>
2024-03-08 21:34:13 +0530

    Update config.py
    Signed-off-by: soulhacker12-pec <155738175+soulhacker12-pec@users.noreply.github.com>

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Soul_Network")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Soul_Network")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/bestfriend_Chatting_Hub")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/bestfriend_Chatting_Hub")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
